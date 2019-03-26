cbae2eff79e430370d710fbd7ea4aee90fa3d687
nyxgear <dev@nyxgear.com>
2019-03-26 12:28:01 +0100

    pbwtzip_speedup_analysis notebook

---
+++ doc/speedup_analysis/utils_execution.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path, PureWindowsPath
+from os.path import basename
+import csv
+import subprocess
+import os
+import shutil
+import numpy as np
+
+application_root = subprocess.check_output(["git rev-parse --show-toplevel"], shell=True, encoding="UTF8")[:-1]
+application_root = Path(application_root)
+build_dir = 'cmake-build-release'
+
+# datasets
+datasets_folder_names = ['large_english_text']
+
+# number of times to repeat the function computation
+# and taking always the minimum result
+repeat_computation_for_results_soundness = 1
+
+# executable paths
+bwtzip_path = application_root / build_dir / 'bwtzip'
+pbwtzip_path = application_root / build_dir / 'pbwtzip'
+pbwtzip2_path = application_root / build_dir / 'pbwtzip2'
+
+# first line of csv with columns titles
+bwtzip_csv_head_line = 'Filename,Chunk_size,Iterations,Times_lasted_longer_function1,Times_lasted_longer_function2,Times_lasted_longer_function3,Times_lasted_longer_function4,Avg_exec_time_function1,Avg_exec_time_function2,Avg_exec_time_function3,Avg_exec_time_function4,Total_compression_time'
+pbwtzip_csv_head_line = 'Filename,Threads_configuration,Chunk_size,Iterations,Times_lasted_longer_read,Times_lasted_longer_stage1,Times_lasted_longer_stage2,Times_lasted_longer_stage3,Times_lasted_longer_write,Avg_exec_time_read,Avg_exec_time_stage1,Avg_exec_time_stage2,Avg_exec_time_stage3,Avg_exec_time_write,Total_compression_time\n'
+pbwtzip2_csv_head_line = 'Filename,Threads_configuration,Chunk_size,Iterations,Times_lasted_longer_read,Times_lasted_longer_stage1,Times_lasted_longer_stage2,Times_lasted_longer_write,Avg_exec_time_read,Avg_exec_time_stage1,Avg_exec_time_stage2,Avg_exec_time_write,Total_compression_time'
+
+
+def get_sequential_compression_times(chunk_sizes, dataset_names=datasets_folder_names,
+                                     repeat_computation=repeat_computation_for_results_soundness):
+    """
+    Compute the total time required to compress all the files in the datasets by using
+    chunk_sizes as parameter space and repeating the computation "repeat_computation"
+    times (for results soundness) by taking always the minimum of the computed sum of compression times.
+    """
+    # remove output dir and its contents
+    output_base = application_root / build_dir / 'compression_output'
+
+    min_compression_time = np.zeros(len(chunk_sizes))
+
+    # repeat t times
+    for t in range(repeat_computation):
+        shutil.rmtree(output_base, ignore_errors=True)
+
+        total_compression_time = np.zeros(len(chunk_sizes))
+        for dataset_name in dataset_names:
+            input_dir = application_root / 'dataset' / dataset_name
+            input_files = [x for x in input_dir.iterdir() if x.is_file()]
+
+            output_dir = output_base / dataset_name
+            # create empty output dir
+            os.makedirs(output_dir)
+
+            for (cnk_idx, chunk_size) in enumerate(chunk_sizes):
+                print(chunk_size)
+                csv_log = output_base / 'bwtzip_{}_{}.csv'.format(dataset_name, chunk_size)
+
+                with open(csv_log, 'w') as csv_file:
+                    csv_file.write(bwtzip_csv_head_line)
+                    for input_file in input_files:
+                        filename = basename(input_file)
+                        output_file = output_dir / (filename + '.bwtzip')
+
+                        args = str(bwtzip_path) + " {} {} {}".format(chunk_size, input_file, output_file)
+                        #print(args)
+                        stats_row = os.popen(args).read()
+                        csv_file.write(stats_row)
+                        # print(stats_row[:-1])
+                        # print(re.split(pattern, stats_row[:-1]))
+                        b = np.genfromtxt([bwtzip_csv_head_line, stats_row], delimiter=',', names=True, dtype=float,
+                                          encoding="UTF8")
+                        # print(b['Total_compression_time'])
+                        total_compression_time[cnk_idx] += b['Total_compression_time']
+        print("Repetition ", t, "performed")
+        print(total_compression_time)
+        if t == 0:
+            min_compression_time = total_compression_time
+        else:
+            min_compression_time = np.minimum(min_compression_time, total_compression_time)
+
+    return min_compression_time
+
+
+def get_parallel_compression_times(pbwtzipx_path,
+                                   pbwtzipx_name,
+                                   chunk_sizes,
+                                   thread_confs,
+                                   csv_head_line,
+                                   dataset_names=datasets_folder_names,
+                                   repeat_computation=repeat_computation_for_results_soundness):
+    """
+    Compute the total time required to compress all the files in the datasets by using
+    chunk_sizes and thread_confs as parameter space and repeating the computation
+    "repeat_computation" times (for results soundness) by taking always the minimum of
+    the computed sum of times.
+    """
+    # remove output dir and its contents
+    output_base = application_root / build_dir / 'compression_output'
+
+    min_compression_time = np.zeros((len(chunk_sizes), len(thread_confs)))
+
+    # repeat t times
+    for t in range(repeat_computation):
+        shutil.rmtree(output_base, ignore_errors=True)
+
+        total_compression_time = np.zeros((len(chunk_sizes), len(thread_confs)))
+        for dataset_name in dataset_names:
+            input_dir = application_root / 'dataset' / dataset_name
+            input_files = [x for x in input_dir.iterdir() if x.is_file()]
+            output_dir = output_base / dataset_name
+            # create empty output dir
+            os.makedirs(output_dir)
+
+            for (cnk_idx, chunk_size) in enumerate(chunk_sizes):
+                for (thr_idx, thread_conf) in enumerate(thread_confs):
+                    print(thread_conf, chunk_size)
+                    csv_log = output_base / '{}_{}_{}_{}.csv'.format(pbwtzipx_name, dataset_name, thread_conf,
+                                                                     chunk_size)
+
+                    with open(csv_log, 'w') as csv_file:
+                        csv_file.write(csv_head_line)
+                        for input_file in input_files:
+                            filename = basename(input_file)
+                            output_file = output_dir / (filename + '.bwtzip')
+
+                            args = str(pbwtzipx_path) + " {} {} {} {}".format(thread_conf, chunk_size, input_file,
+                                                                              output_file)
+                            # print(args)
+                            stats_row = os.popen(args).read()
+                            # stats_row = subprocess.check_output([args], shell=True, encoding="UTF8")
+                            csv_file.write(stats_row)
+                            # print(stats_row[:-1])
+                            # print(re.split(pattern, stats_row[:-1]))
+                            b = np.genfromtxt([csv_head_line, stats_row], delimiter=',', names=True,
+                                              dtype=float, encoding="UTF8")
+                            # print(b['Total_compression_time'])
+                            total_compression_time[cnk_idx][thr_idx] += b['Total_compression_time']
+        print("Repetition ", t, "performed")
+        print(total_compression_time)
+        if t == 0:
+            min_compression_time = total_compression_time
+        else:
+            min_compression_time = np.minimum(min_compression_time, total_compression_time)
+
+    return min_compression_time
+
+
+def get_pbwtzip_parallel_compression_times(chunk_sizes, thread_confs, dataset_names=datasets_folder_names):
+    return get_parallel_compression_times(pbwtzip_path,
+                                          "pbwtzip",
+                                          chunk_sizes,
+                                          thread_confs,
+                                          pbwtzip_csv_head_line,
+                                          dataset_names)
+
+
+def get_pbwtzip2_parallel_compression_times(chunk_sizes, thread_confs, dataset_names=datasets_folder_names):
+    return get_parallel_compression_times(pbwtzip_path2,
+                                          "pbwtzip2",
+                                          chunk_sizes,
+                                          thread_confs,
+                                          pbwtzip2_csv_head_line,
+                                          dataset_names)
---
+++ doc/speedup_analysis/utils_visualization.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
+scale_factor = 2
+figsize_line_chart = (6.4 * scale_factor, 4.0 * scale_factor)
+figsize_heat_map = (6.4 * scale_factor, 4.0 * scale_factor)
+figsize_3d_bar = (15 * scale_factor, 4.0 * scale_factor)
+
+
+def get_vis_cnk_sizes(chunk_sizes):
+    return ['{0:.0f} Kb'.format(x / 1024) for x in chunk_sizes]
+
+
+def plot_line_chart(title, parameters_matrix, chunk_sizes, thread_confs):
+    plt.figure(figsize=figsize_line_chart)
+
+    max_p = float('-inf')
+    min_p = float('inf')
+
+    for i, r in enumerate(parameters_matrix):
+        plt.plot(thread_confs, r, 'C' + str(i) + 'o-', label='{} Kb'.format(int(chunk_sizes[i] / 1024)))
+
+        max_p = max(max(r), max_p)
+        min_p = min(min(r), min_p)
+        # for t in zip(thread_confs, r):
+        #    fmt = (t[0], round(t[1], 3))
+        #    dd = (t[0], t[1] + 0.01)
+        #    ax.annotate('%s, %s' % fmt, xy=dd, textcoords='data')
+
+    plt.title(title, fontsize=16, pad=15)
+    plt.grid(True)
+    plt.ylim(min_p - 0.2, max_p + 0.2)
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+def plot_heatmap(title, data, chunk_sizes, thread_confs, cmap, text_color):
+    fig = plt.figure(figsize=figsize_heat_map)
+    ax = fig.add_subplot(111)
+    im = ax.imshow(data, cmap=cmap)
+
+    farmers = thread_confs
+    vegetables = chunk_sizes
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(thread_confs)))
+    ax.set_yticks(np.arange(len(chunk_sizes)))
+
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(thread_confs, fontsize=12)
+    ax.set_yticklabels(chunk_sizes, fontsize=12)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(vegetables)):
+        for j in range(len(farmers)):
+            text = ax.text(j, i, round(data[i, j], 2), fontsize=16, ha="center", va="center", color=text_color)
+
+    ax.set_title(title, fontsize=16, pad=15)
+    fig.tight_layout()
+    plt.colorbar(im)
+    plt.show()
+
+
+def plot_3d_bar(title, data, chunk_sizes, thread_confs, color):
+    fig = plt.figure(figsize=figsize_3d_bar)
+    ax = fig.add_subplot(121, projection='3d')
+
+    x = np.arange(len(chunk_sizes))
+    y = np.arange(len(thread_confs))
+    _x = np.arange(len(chunk_sizes))
+    _y = np.arange(len(thread_confs))
+    _xx, _yy = np.meshgrid(_x, _y)
+    x, y = _xx.ravel(), _yy.ravel()
+
+    dz = data.flatten(order='C')
+    z = np.zeros_like(dz)
+    dx = 0.4
+    dy = 0.2
+
+    ax.bar3d(x, y, z, dx, dy, dz, color=color, shade=True)
+
+    ax.set_title(title, pad=25, fontsize=15)
+
+    # set ticks
+    ax.w_xaxis.set_ticks(np.arange(dx / 2, len(chunk_sizes)))
+    ax.w_yaxis.set_ticks(np.arange(dy / 2, len(thread_confs)))
+
+    # set tick labels
+    ax.set_xticklabels(chunk_sizes)
+    ax.set_yticklabels(thread_confs)
+
+    ax.set_xlabel("Chunk size", labelpad=20, fontsize=14)
+    ax.set_ylabel("Thread conf", labelpad=20, fontsize=14)
+    ax.set_zlabel(title, labelpad=20, fontsize=14, rotation='vertical')
+
+    ax.set_zlim3d(0.0)
+
+    _x_ticks = ax.w_xaxis.get_ticklocs()
+    _y_ticks = ax.w_xaxis.get_ticklocs()
+    _xx, _yy = np.meshgrid(_x_ticks, _y_ticks)
+    x, y = _xx.ravel(), _yy.ravel()
+
+    for i, t in enumerate(dz):
+        lbl = str(round(t, 3))
+        if title == 'Time':
+            lbl += ' s'
+        ax.text(x[i] - (dx / 4), y[i] - (dy / 4), t, lbl, color='k', fontsize=12, backgroundcolor=(1, 1, 1, 0.8))
+
+    ax.view_init(azim=-60, elev=55)
+
+    plt.show()
